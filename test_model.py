1e7737c39505a0d85c2b01b2e3a0e05d29581022
Leo Moon <haha978@naver.com>
2024-10-21 22:40:51 -0700

    initial test model to classify brain tumor dataset

---
+++ test_model.py
@@ -0,0 +1,69 @@
+import numpy as np
+import os
+import glob
+import numpy as np
+from PIL import Image
+import h5py
+
+
+
+def generate_h5py_database():
+    # Set paths to image directories
+    img_dir_type1 = '/home/leom/code/Brain_Tumor_MRI_Image_Dataset/Training/glioma/'
+    img_dir_type2 = '/home/leom/code/Brain_Tumor_MRI_Image_Dataset/Training/pituitary/'
+
+    test_dir_type1 = '/home/leom/code/Brain_Tumor_MRI_Image_Dataset/Testing/glioma/'
+    test_dir_type2 = '/home/leom/code/Brain_Tumor_MRI_Image_Dataset/Testing/pituitary/'
+    
+    # Create lists to store image data and labels
+    train_img_l = []
+    test_img_l = []
+    train_label_l = []
+    test_label_l = []
+    
+    for img_path in glob.glob(os.path.join(img_dir_type1, '*.jpg')):
+        img = Image.open(img_path)
+        img = np.array(img)/255
+        train_img_l.append(img)
+        train_label_l.append(0)
+    
+    for img_path in glob.glob(os.path.join(img_dir_type2, '*.jpg')):
+        img = Image.open(img_path)
+        img = np.array(img) / 255.0  # Normalize pixel values
+        train_img_l.append(img)
+        train_label_l.append(1)  # Label for type 2 images
+        
+    for img_path in glob.glob(os.path.join(test_dir_type1, '*.jpg')):
+        img = Image.open(img_path)
+        img = np.array(img)/255
+        test_img_l.append(img)
+        test_label_l.append(0)
+    
+    for img_path in glob.glob(os.path.join(test_dir_type2, '*.jpg')):
+        img = Image.open(img_path)
+        img = np.array(img) / 255.0  # Normalize pixel values
+        test_img_l.append(img)
+        test_label_l.append(1)  # Label for type 2 images
+
+    output_dir = "/home/leom/code/Brain_Tumor_MRI_Image_hdf5/"
+    
+    # Convert lists to numpy arrays
+    train_img_data = np.array(train_img_l)
+    test_img_data = np.array(test_img_l)
+    train_label_l = np.array(train_label_l)
+    test_label_l = np.array(test_label_l)
+
+    # Create h5py files
+    with h5py.File(os.path.join(output_dir, 'training.h5'), 'w') as f:
+        f.create_dataset('images', data=train_img_data)
+        f.create_dataset('labels', data=train_label_l)
+
+    with h5py.File(os.path.join(output_dir, 'test.h5'), 'w') as f:
+        f.create_dataset('images', data=test_img_data)
+        f.create_dataset('labels', data=test_label_l)    
+
+def main():
+    generate_h5py_database()
+
+if __name__ == '__main__':
+    main()
