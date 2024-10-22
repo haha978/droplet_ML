40515e68200470865efec0a9a1ee271fa6284258
Leo Moon <haha978@naver.com>
2024-10-22 16:43:45 -0700

    added Dataset class

---
+++ Image_dataset.py
@@ -0,0 +1,24 @@
+import h5py
+import torch
+from torch.utils.data import Dataset
+
+class ImageDataset(Dataset):
+    def __init__(self, h5_file_path, transform=None):
+        self.h5_path = h5_file_path
+        self.h5_file = h5py.File(self.h5_path, 'r')
+        self.transform = transform
+        self.images = self.h5_file['images']
+        self.labels = self.h5_file['labels']
+        
+    def __len__(self):
+        return self.images.shape[0]
+    
+    def __getitem__(self, idx):
+        img = self.images['images'][idx]
+        if self.transform is not None:
+            img = self.transform(img)
+        label = self.labels['labels'][idx]
+        return img, label
+
+    def close(self):
+        self.h5_file.close()
