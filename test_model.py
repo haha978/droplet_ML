af718a57f0075ededb0882acc20f72d7b721a733
Leo Moon <haha978@naver.com>
2024-10-22 17:54:57 -0700

    modularized database code

--- test_model.py
+++ test_model.py
@@ -4,66 +4,19 @@ import glob
 import numpy as np
 from PIL import Image
 import h5py
+import torch
 
+def test():
+    pass
 
-
-def generate_h5py_database():
-    # Set paths to image directories
-    img_dir_type1 = '/home/leom/code/Brain_Tumor_MRI_Image_Dataset/Training/glioma/'
-    img_dir_type2 = '/home/leom/code/Brain_Tumor_MRI_Image_Dataset/Training/pituitary/'
-
-    test_dir_type1 = '/home/leom/code/Brain_Tumor_MRI_Image_Dataset/Testing/glioma/'
-    test_dir_type2 = '/home/leom/code/Brain_Tumor_MRI_Image_Dataset/Testing/pituitary/'
-    
-    # Create lists to store image data and labels
-    train_img_l = []
-    test_img_l = []
-    train_label_l = []
-    test_label_l = []
-    
-    for img_path in glob.glob(os.path.join(img_dir_type1, '*.jpg')):
-        img = Image.open(img_path)
-        img = np.array(img)/255
-        train_img_l.append(img)
-        train_label_l.append(0)
-    
-    for img_path in glob.glob(os.path.join(img_dir_type2, '*.jpg')):
-        img = Image.open(img_path)
-        img = np.array(img) / 255.0  # Normalize pixel values
-        train_img_l.append(img)
-        train_label_l.append(1)  # Label for type 2 images
-        
-    for img_path in glob.glob(os.path.join(test_dir_type1, '*.jpg')):
-        img = Image.open(img_path)
-        img = np.array(img)/255
-        test_img_l.append(img)
-        test_label_l.append(0)
-    
-    for img_path in glob.glob(os.path.join(test_dir_type2, '*.jpg')):
-        img = Image.open(img_path)
-        img = np.array(img) / 255.0  # Normalize pixel values
-        test_img_l.append(img)
-        test_label_l.append(1)  # Label for type 2 images
-
-    output_dir = "/home/leom/code/Brain_Tumor_MRI_Image_hdf5/"
-    
-    # Convert lists to numpy arrays
-    train_img_data = np.array(train_img_l)
-    test_img_data = np.array(test_img_l)
-    train_label_l = np.array(train_label_l)
-    test_label_l = np.array(test_label_l)
-
-    # Create h5py files
-    with h5py.File(os.path.join(output_dir, 'training.h5'), 'w') as f:
-        f.create_dataset('images', data=train_img_data)
-        f.create_dataset('labels', data=train_label_l)
-
-    with h5py.File(os.path.join(output_dir, 'test.h5'), 'w') as f:
-        f.create_dataset('images', data=test_img_data)
-        f.create_dataset('labels', data=test_label_l)    
+def train():
+    pass
 
 def main():
-    generate_h5py_database()
+    print(torch.cuda.is_available())
+    print(torch.cuda.get_device_name(0))
+    print(torch.version.cuda)
+    # os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 
 if __name__ == '__main__':
     main()
